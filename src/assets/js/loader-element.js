7d258d1bfe9960499859bd00866bb63aa7911e3d
Jalal Jaleh <JalalJaleh@gmail.com>
2022-10-01 23:08:09 +0330

    add blog.

---
+++ src/assets/js/loader-element.js
@@ -0,0 +1,43 @@
+﻿function load_footer() {
+    fetch('/src/assets/elements/footer.html')
+        .then(function (response) {
+            return response.text();
+        }).then(function (txt) {
+            var div = document.getElementById('footer');
+            div.innerHTML += txt;
+        });
+}
+
+function load_header(background, image, title, tagline, mode) {
+    fetch('/src/assets/elements/header.html')
+        .then(function (response) {
+            return response.text();
+        }).then(function (txt) {
+
+            if (background == null) {
+                background = "assets/images/background.jpg";
+            }
+            if (image == null) {
+                image = "assets/images/person-green.jpg";
+            }
+            if (title == null) {
+                title = "Jalal Jaleh"
+            }
+            if (tagline == null) {
+                tagline = "Mohammed Jalal Jaleh";
+            }
+            if (mode == null) {
+                mode = "center";
+            }
+
+            txt = txt
+                .replace("{image}", image)
+                .replace("{background}", background)
+                .replace("{title}", title)
+                .replace("{tagline}", tagline)
+                .replace("{mode}", mode);
+
+            var div = document.getElementById('header');
+            div.innerHTML = txt;
+        });
+}
---
+++ src/assets/js/loader-post.js
@@ -0,0 +1,41 @@
+﻿async function load_posts() {
+    var data = await get_all_posts();
+    var div = document.getElementById('post-content');
+    data.forEach(function (object) { load_post(div, object) });
+}
+
+function load_post(element, post) {
+    element.innerHTML += `
+                    <article class="post">
+                        <header class="entry-header">
+                            <div class="entry-meta">
+                                <span class="posted-on"><time class="entry-date published" date="2022-10-01">`+ post.time + `</time></span>
+                            </div>
+                            <h1 class="entry-title"><a href="/src/post.html?postId=`+ post.id + `" rel="bookmark">` + post.title + `</a></h1>
+                        </header>
+                        <div class="entry-content">
+                            `+ post.content + `
+                        </div>
+                    </article>
+`;
+}
+
+async function load_page_post() {
+    var postId = /postId=([^&]+)/.exec(location.search)[1];
+    var element = document.getElementById('post-id');
+
+    var post = await get_post(postId);
+    load_post(element, post);
+}
+
+
+async function get_all_posts() {
+    var data = await fetch('/src/assets/data/posts.json');
+    var json = await data.json();
+    return json;
+}
+
+async function get_post(id) {
+    var data = await get_all_posts();
+    return data[0];
+}
