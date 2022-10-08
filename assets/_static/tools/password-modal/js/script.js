978c48257077bc3946ddd629b8226baf5478309e
Jalal Jaleh <JalalJaleh@gmail.com>
2022-10-08 13:21:33 +0330

    update

---
+++ assets/_static/tools/password-modal/js/script.js
@@ -0,0 +1,58 @@
+﻿function closeModal(modal, pass,funaction) {
+    modal.style.display = "none";
+    var password = document.getElementById("labelPassword").value;
+    if (password == pass) {
+        funaction();
+    }
+}
+
+
+function createModal() {
+    var body = document.getElementsByTagName("body")[0];
+
+    body.innerHTML +=
+        `
+    <div id="myModal" class="modal">
+        <div class="modal-content">
+            <p>جهت ورود به وبلاگ رمز عبور را وارد کنید.</p>
+            <input class="modal-button" id="labelPassword" placeholder="رمز عبور">
+                <button class="modal-button" id="btnSubmitPassword">ثبت</button>
+        </div>
+
+    </div>
+`;
+}
+
+function showModal(pass, funaction) {
+
+    createModal();
+
+    var modal = document.getElementById("myModal");
+    modal.style.display = "block";
+
+
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            closeModal(modal, pass, funaction);
+        }
+    }
+
+    var btnCloseModal = document.getElementById("btnSubmitPassword");
+    btnCloseModal.onclick = function () {
+        closeModal(modal, pass, funaction)
+    };
+
+};
+
+
+function initialize(pass, funaction) {
+
+    var fileref = document.createElement("link");
+    fileref.rel = "stylesheet";
+    fileref.type = "text/css";
+    fileref.href = "https://jalaljaleh.github.io/assets/_static/tools/password-modal/css/style.css";
+    document.getElementsByTagName("head")[0].appendChild(fileref);
+
+    showModal(pass, funaction);
+}
+
