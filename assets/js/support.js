63fde444001418ee37666444bb5764d3f2311303
Mohammed Jalal Jaleh <jalaljaleh@gmail.com>
2022-10-21 21:28:57 +0330

    Update support.js

--- assets/js/support.js
+++ assets/js/support.js
@@ -1,5 +1,1 @@
-<!---start GOFTINO code--->
 
-  !function(){var i="PcIJ2j",a=window,d=document;function g(){var g=d.createElement("script"),s="https://www.goftino.com/widget/"+i,l=localStorage.getItem("goftino_"+i);g.async=!0,g.src=l?s+"?o="+l:s;d.getElementsByTagName("head")[0].appendChild(g);}"complete"===d.readyState?g():a.attachEvent?a.attachEvent("onload",g):a.addEventListener("load",g,!1);}();
-
-<!---end GOFTINO code--->
