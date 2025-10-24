1db60bd0d58b2f53f353814daf7d42f7cf549a17
Jalal Jaleh <jalaljaleh@gmail.com>
2025-10-24 13:55:19 +0330

    +

--- api/router.js
+++ api/router.js
@@ -1,13 +1,9 @@
 // src/router.js
 import { AutoRouter } from 'itty-router';
-import healthController from './controllers/health/ping.js';
 import devController from './controllers/developer/handler.js';
 
 
 const router = AutoRouter();
-
-// health
-router.get('/ping', healthController);
 
 // dev operations
 router.get('/dev', devController);
