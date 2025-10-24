f4344e05e65b1a9c9711be76a8a8c2557bf3f731
Jalal Jaleh <jalaljaleh@gmail.com>
2025-10-24 15:06:09 +0330

    ++

--- api/router.js
+++ api/router.js
@@ -1,12 +1,17 @@
 // src/router.js
 import { AutoRouter } from 'itty-router';
 import devController from './controllers/developer/handler.js';
+import notifyController from './handlers/telegram.js';
 
 
 const router = AutoRouter();
 
 // dev operations
 router.get('/dev', devController);
+
+// cross-origin notify endpoint (POST)
+router.post('/telegram-notify', notifyController);
+
 
 router.all('*', () =>
     Response.redirect('https://jalaljaleh.github.io/', 302)
